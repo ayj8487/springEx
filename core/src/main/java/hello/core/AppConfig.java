fde1560d05046b39c1f4a283f7f1334dc52ec45b
ayj8487 <ayj8487@naver.com>
2022-07-07 10:59:17 +0900

    스프링으로 코드 전환

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
@@ -10,24 +13,29 @@ import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
+@Configuration // 설정정보라는 명시 어노테이션
 public class AppConfig {
 	//환경 구성 총괄
+
 	
+	@Bean // 스프링 컨테이너에 등록 어노테이션
 	public MemberService memberService() {
 		return new MemberServiceImpl(memberRepository());
 		// 생성자 주입 
 	}
-	private MemberRepository memberRepository() {
+	
+	@Bean
+	public MemberRepository memberRepository() {
 		return new MemoryMemberRepository();
 	}
-	
+	@Bean
 	public OrderService orderService() {
 		return new OrderServiceImpl(memberRepository(), discountPolicy());
 	}
 
 //	사용영역 OrderServiceImpl 의 코드를 손 대지 않고 
 //	구성영역 AppConfig만 수정해서 할인률을 변경할 수있게 되었다.
-
+	@Bean
 	public DiscountPolicy discountPolicy() { // 고정할인 => 정률할인 변경시 밑 코드만 변경하면 된다.
 		return new RateDiscountPolicy();
 //		return new FixDiscountPolicy();
